7a46cf3e9efb8158a0d01620862051555515222e
minju <71gnos@naver.com>
2024-03-13 23:15:51 +0900

    p

--- lib/main.dart
+++ lib/main.dart
@@ -14,104 +14,73 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.black,
           centerTitle: true,
-          title: Text(
-            'Instagram',
-            style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'fonts/FiraSansCondensed-Black.ttf',
-                fontSize: 28
-            ),
+          title: Text('Instagram',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'fonts/FiraSansCondensed-Black.ttf',
+            fontSize: 28
+          ),
           ),
         ),
         body: Column(
           children: [
             Expanded(
-                flex:1,
-                child: Container(
-                  color: Colors.grey[200],
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('images/cat2.webp'),
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('images/cat1.png'),
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('images/cat3.jpg'),
-                      )
-                    ],
+              flex: 1,
+            child: Container(
+              color: Colors.grey[200],
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('images/cat1.png'),
                   ),
-                )),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('images/cat2.webp'),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('images/cat3.jpg'),
+                  )
+                ],
+              ),
+            ),),
             Expanded(
-                flex:8,
-                child: Container(
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(15),
-                        child: Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 30,
-                              child: FlutterLogo(
-                                size: 30,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Text(
-                              'mingreen',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Spacer(),
-                            Icon(Icons.more_horiz)
-                          ],
-                        ),
-                      ),
-                      Flexible(
-                          child: Container(
-                            padding: EdgeInsets.all(15),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset('images/cat4.webp'),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.favorite_border),
-                                    SizedBox(width: 8),
-                                    Icon(Icons.comment),
-                                    SizedBox(width: 8),
-                                    Icon(Icons.bookmark_border),
-                                  ],
-                                ),
-                                Text(
-                                  'Liked by more than 5',
-                                ),
-                              ],
-                            ),
-                          )
-                      )
-                    ],
-                  ),
-                ))
+              flex: 8,
+              child: Container(
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  children: [
+                    CircleAvatar(radius: 30,
+                      child: FlutterLogo(size: 30,),
+                    ),
+                    SizedBox(width: 15,),
+                    Text('m.mingreen',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    Icon(Icons.more_horiz)
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                padding: EdgeInsets.all(15),
+                child: Column(
+
+                ),
+              ),
+            )
           ],
         ),
-      ),
+      )
     );
   }
 }
